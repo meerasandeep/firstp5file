4d1ae990913bc949e3e65adaa3e46a99cb6bbbd3
meerasandeep <32289993+meerasandeep@users.noreply.github.com>
2022-10-06 18:56:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+tint_color = "";
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(640,480);
+    canvas.position(110,250);
+    video = createCapture(VIDEO);
+    video.hide();
+    
+}
+function draw(){
+  image(video,0,0,640,480);
+    tint(tint_color);
+}
+function take_snapshot(){
+save('hbday.png');
+}
+function filter_tint(){
+    tint_color = document.getElementById("color_name").value
+}
